9d782fa106b9eb82908957936466f5222450f0fe
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-06-22 22:48:08 -0600

    Add canv JS file, canv size is now determined by JS variable

--- app/assets/javascripts/attr.js
+++ app/assets/javascripts/attr.js
@@ -23,12 +23,6 @@ $(document).ready(function() {
 		$('.active').attr('stroke-linejoin', linejoin);
 	});
 
-	$('#prim a').click(function() {
-		var d = $(this).find('path').attr('d');
-
-		$('#canv').append('<svg class="active" style="margin-left: ' + pos[0] + '; margin-top: ' + pos[1] + '"><path d="' + d + '"></path></svg>');
-	});
-
 	$('#layer input').change(function() {
 		$('.active').attr('z-index', $(this).val());
 	});
---
+++ app/assets/javascripts/canv.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#canv').css({
+		width: size[0] + 'px',
+		height: size[1] + 'px'
+	});
+});
