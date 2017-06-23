313262db50a0d764157d177db22fd81154354d07
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-06-23 00:48:26 -0600

    Add attribute input to change visibility prominence (like z-index). Changes position in DOM because that's how SVG visibility is determined. Hopefully to be replaced with a plugin or jQuery feature I've yet to discover

--- app/assets/javascripts/attr.js
+++ app/assets/javascripts/attr.js
@@ -26,4 +26,15 @@ $(document).ready(function() {
 	$('#layer input').change(function() {
 		$('.active').attr('z-index', $(this).val());
 	});
+
+	$('#z-pos input').change(function() {
+		// z-index doesn't work on SVG; visibility is determined by position in DOM
+		var zPos = $(this).val();
+
+		var el = $('.active').clone();
+
+		$('.active').remove();
+
+		zPos > 0 ? $('.obj:eq(' + (zPos - 1) + ')').after(el) : $('#canv').prepend(el)
+	});
 });
